164a2ec8dfcb9e2f1ddebe9a20bdd0c1c09a79c8
Yuri Vyunik <Yuri.Vyunik@rztech.us>
2020-11-23 15:44:32 +0200

    init commit

---
+++ src/OmsApi/Controllers/IdentityController.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("identity")]
+    [Authorize()]
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
